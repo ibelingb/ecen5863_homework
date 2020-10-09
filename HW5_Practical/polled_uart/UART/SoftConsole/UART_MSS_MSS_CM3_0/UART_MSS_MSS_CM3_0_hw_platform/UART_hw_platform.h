129ce85cfe6704d995b01777b90642d557d30780
Brian Ibeling <bibeling@gmail.com>
2020-10-08 22:49:18 -0600

    Adding files for HW5_Practical.

---
+++ HW5_Practical/polled_uart/UART/SoftConsole/UART_MSS_MSS_CM3_0/UART_MSS_MSS_CM3_0_hw_platform/UART_hw_platform.h
@@ -0,0 +1,16 @@
+#ifndef UART_HW_PLATFORM_H_
+#define UART_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Microsemi SmartDesign  Thu Oct 08 22:40:08 2020
+*
+*Memory map specification for peripherals in UART
+*/
+
+/*-----------------------------------------------------------------------------
+* MSS_CM3_0 subsystem memory map
+* Master(s) for this subsystem: MSS_CM3_0 
+*---------------------------------------------------------------------------*/
+
+
+#endif /* UART_HW_PLATFORM_H_*/
